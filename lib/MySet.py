d2f842af334fbdc3ff34adc7d78697f95d1421b1
Elise Erickson <elisece717@gmail.com>
2024-02-21 17:21:18 -0500

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,29 @@
 class MySet:
 
-    pass
+    def __init__(self, collection = []):
+        self.dictionary = {}
+        for value in collection:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self.dictionary
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+        return self.dictionary
+    
+    def __str__(self):
+        return "MySet: {" + ','.join(f'{key}' for key in self.dictionary.keys()) + "}"
+    
+
